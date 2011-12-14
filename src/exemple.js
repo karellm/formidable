8b8011ff78e1815c670fe73a58eede19116a776e
Karel Ledru-Mathe <karel@ledrumathe.com>
2011-12-14 14:51:58 -0500

    Exemple of a form configuration

---
+++ src/exemple.js
@@ -0,0 +1,53 @@
+
+new Backbone.Form({
+    data: { id: 123, name: 'Rod Kimble', password: 'cool beans' }, //Data to populate the form with
+    schema: {
+        // FIELD
+        id: { 
+          // type: 'Number'
+          editor: {
+            type: 'Number'
+            schema: {
+              
+            }
+          }
+          // OR
+          editor: 'Number'
+
+          append: true, // default true, false if el is passed
+          class
+          id // id for the field
+          title // title of the label
+          el // el if there is an existing dom element
+          validator // array of validators
+          idPrefix // prefix id to use for the fields and label
+          template: 'templatename' // reference one of the templates
+        },
+
+        // FIELD
+        name:       {},
+        
+        // FIELD
+        password:   { type: 'Password' }
+    }
+    model
+    fieldsets: [
+      ['id'],
+      ['name', 'password']
+    ]
+    // OR
+    fieldsets: {
+      legend: 'Legend',
+      fields: [
+        ['id'],
+        ['name', 'password']
+      ]
+    },
+
+    templates: {
+      templatename: '<div></div>'
+    }
+
+    // fields
+    // idPrefix
+})
