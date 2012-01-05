77dd6a82104eeac65c7043c9b9aa212aa8d6e3ea
Karel Ledru-Mathe <karel@ledrumathe.com>
2012-01-04 20:25:54 -0500

    Add a form-messages library for validators

---
+++ src/backbone-form-messages.js
@@ -0,0 +1,110 @@
+
+define({
+
+
+  // ENGLISH
+  en: {
+    required: {
+      pass: null,
+      fail: "can't be blank"
+    },
+    number: {
+      pass: null,
+      fail: "must be a number"
+    },
+    integer: {
+      pass: null,
+      fail: "must be an integer"
+    },
+    min: {
+      pass: null,
+      fail: "must be at least %s"
+    },
+    max: {
+      pass: null,
+      fail: "must be at the most %s"
+    },
+    minLength: {
+      pass: null,
+      fail: "must have at least %s characters"
+    },
+    maxLength: {
+      pass: null,
+      fail: "must have at the most %s characters"
+    },
+    in: {
+      pass: null,
+      fail: "must be in %s"
+    },
+    pattern: {
+      pass: null,
+      fail: "isn't a valid value"
+    },
+    email: {
+      pass: null,
+      fail: "must be a valid email"
+    },
+    url: {
+      pass: null,
+      fail: "must be a valid url"
+    },
+    ip: {
+      pass: null,
+      fail: "must be a valid ip"
+    }
+  },
+
+
+  // FRENCH
+  fr: {
+    required: {
+      pass: null,
+      fail: "ne peut être vide"
+    },
+    number: {
+      pass: null,
+      fail: "doit être un nombre"
+    },
+    integer: {
+      pass: null,
+      fail: "doit être un entier"
+    },
+    min: {
+      pass: null,
+      fail: "doit être au moins %s"
+    },
+    max: {
+      pass: null,
+      fail: "doit être au plus %s"
+    },
+    minLength: {
+      pass: null,
+      fail: "doit avoir au moins %s charactères"
+    },
+    maxLength: {
+      pass: null,
+      fail: "doit avoir au plus %s charactères"
+    },
+    in: {
+      pass: null,
+      fail: "doit être %s"
+    },
+    pattern: {
+      pass: null,
+      fail: "n'est pas une valeur valide"
+    },
+    email: {
+      pass: null,
+      fail: "doit être un courriel valide"
+    },
+    url: {
+      pass: null,
+      fail: "doit être une url valide"
+    },
+    ip: {
+      pass: null,
+      fail: "doit être une ip valide"
+    }
+  }
+
+});
