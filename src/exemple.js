9e99c0d7433255e622698968830ccd1e8c3a0447
Karel Ledru-Mathe <karel@ledrumathe.com>
2012-01-13 16:12:14 -0500

    Comment out the exemple

--- src/exemple.js
+++ src/exemple.js
@@ -1,61 +1,61 @@
 
-new Backbone.Form({
-    data: { id: 123, name: 'Rod Kimble', password: 'cool beans' }, //Data to populate the form with
-    schema: {
-        // FIELD
-        id: { 
-          // type: 'Number'
-          editor: {
-            type: 'Number'
-            el: 
-            schema: {
+// new Backbone.Form({
+//     data: { id: 123, name: 'Rod Kimble', password: 'cool beans' }, //Data to populate the form with
+//     schema: {
+//         // FIELD
+//         id: { 
+//           // type: 'Number'
+//           editor: {
+//             type: 'Number'
+//             el: 
+//             schema: {
               
-            }
-          }
-          // OR
-          editor: 'Number'
+//             }
+//           },
+//           OR
+//           editor: 'Number'
 
-          append: true, // default true, false if el is passed
-          class
-          id // id for the field
-          title // title of the label
-          el // el if there is an existing dom element
-          validators: {
-              required: true,
-              minLength: 3,
-              maxLength: 10,
-              url: true, // true or a regex
-            }
-          idPrefix // prefix id to use for the fields and label
-          template: 'templatename' // reference one of the templates
-        },
+//           append: true, // default true, false if el is passed
+//           class
+//           id // id for the field
+//           title // title of the label
+//           el // el if there is an existing dom element
+//           validators: {
+//               required: true,
+//               minLength: 3,
+//               maxLength: 10,
+//               url: true, // true or a regex
+//             }
+//           idPrefix // prefix id to use for the fields and label
+//           template: 'templatename' // reference one of the templates
+//         },
 
-        // FIELD
-        name:       {},
+//         // FIELD
+//         name:       {},
         
-        // FIELD
-        password:   { type: 'Password' }
-    }
-    model
-    fieldsets: [
-      ['id'],
-      ['name', 'password']
-    ]
-    // OR
-    fieldsets: {
-      legend: 'Legend',
-      fields: [
-        ['id'],
-        ['name', 'password']
-      ]
-    },
-    errorClass: 's-error', // default s-error. On validation gets added if the test fails
-    successClass: '', // empty by default. On validation gets added if the test passes
+//         // FIELD
+//         password:   { type: 'Password' }
+//     }
+//     model
+//     fieldsets: [
+//       ['id'],
+//       ['name', 'password']
+//     ]
+//     // OR
+//     fieldsets: {
+//       legend: 'Legend',
+//       fields: [
+//         ['id'],
+//         ['name', 'password']
+//       ]
+//     },
+//     errorClass: 's-error', // default s-error. On validation gets added if the test fails
+//     successClass: '', // empty by default. On validation gets added if the test passes
 
-    templates: {
-      templatename: '<div></div>'
-    }
+//     templates: {
+//       templatename: '<div></div>'
+//     }
 
-    // fields
-    // idPrefix
-})
+//     // fields
+//     // idPrefix
+// })
